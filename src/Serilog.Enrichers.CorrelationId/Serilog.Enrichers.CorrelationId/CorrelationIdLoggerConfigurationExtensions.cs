2c56e0694cce1e579e9bd203e1b02576a5a7a797
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-12 11:02:20 +0100

    Added a simple enricher that populates the current HttpContext items with a unique ID.

---
+++ src/Serilog.Enrichers.CorrelationId/Serilog.Enrichers.CorrelationId/CorrelationIdLoggerConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Enrichers;
+
+namespace Serilog
+{
+    public static class CorrelationIdLoggerConfigurationExtensions
+    {
+        public static LoggerConfiguration WithCorrelationId(this LoggerEnrichmentConfiguration enrichmentConfiguration)
+        {
+            if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
+            return enrichmentConfiguration.With<CorrelationIdEnricher>();
+        }
+    }
+}
---
+++ src/Serilog.Enrichers.CorrelationId/Serilog.Enrichers.CorrelationId/Enrichers/CorrelationIdEnricher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Enrichers
+{
+    public class CorrelationIdEnricher : ILogEventEnricher
+    {
+        private const string CorrelationIdPropertyName = "CorrelationId";
+        private static readonly string CorrelationIdItemName = $"{typeof(CorrelationIdEnricher).Name}+CorrelationId";
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+
+            if (HttpContext.Current == null)
+                return;
+
+            var correlationId = GetCorrelationId();
+            var correlationIdProperty = new LogEventProperty(CorrelationIdPropertyName, new ScalarValue(correlationId));
+
+            logEvent.AddPropertyIfAbsent(correlationIdProperty);
+        }
+
+        private static Guid GetCorrelationId()
+        {
+            return (Guid) (HttpContext.Current.Items[CorrelationIdItemName] ??
+                           (HttpContext.Current.Items[CorrelationIdItemName] = Guid.NewGuid()));
+        }
+    }
+}
