81119fb65b36ca9f4c3434ddc8f70f103a8e1b85
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-12 11:02:33 +0100

    Added test for the CorrelationIdEnricher.

---
+++ src/Serilog.Enrichers.CorrelationId/Serilog.Enrichers.CorrelationId.Tests/Enrichers/CorrelationIdEnricherTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using NUnit.Framework;
+using Serilog.Events;
+using Serilog.Tests.Support;
+
+namespace Serilog.Tests.Enrichers
+{
+    [TestFixture]
+    [Parallelizable]
+    public class CorrelationIdEnricherTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            HttpContext.Current = new HttpContext(
+                new HttpRequest("test", "https://serilog.net/", ""),
+                new HttpResponse(new StringWriter()));
+        }
+
+        [Test]
+        public void CorrelationIdEnricherIsApplied()
+        {
+            LogEvent evt = null;
+            var log = new LoggerConfiguration()
+                .Enrich.WithCorrelationId()
+                .WriteTo.Sink(new DelegateSink.DelegatingSink(e => evt = e))
+                .CreateLogger();
+
+            log.Information(@"Has a CorrelationId property");
+
+            Assert.NotNull(evt);
+            Assert.NotNull((Guid) evt.Properties["CorrelationId"].LiteralValue());
+        }
+    }
+}
