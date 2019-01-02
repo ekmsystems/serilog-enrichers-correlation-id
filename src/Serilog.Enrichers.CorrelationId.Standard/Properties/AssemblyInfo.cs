604d45f84b170b3fe5f1246e33b2d729f00a91d9
Steve Atkinson <steve.atkinson@dootrix.com>
2019-01-02 13:41:54 +0000

    Added tests for the Standard project.

---
+++ src/Serilog.Enrichers.CorrelationId.Standard/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo("Serilog.Enrichers.CorrelationId.Standard.Tests")]
--- src/Serilog.Enrichers.CorrelationId/Properties/AssemblyInfo.cs
+++ src/Serilog.Enrichers.CorrelationId/Properties/AssemblyInfo.cs
@@ -38,3 +38,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Serilog.Enrichers.CorrelationId.Tests")]
