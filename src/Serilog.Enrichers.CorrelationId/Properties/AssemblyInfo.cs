fa7b35f3165c65ff8b887e97e740b7ef02bb8cb5
Elliot Coyle <elliotcoyle@ekmsystems.co.uk>
2019-11-04 14:47:20 +0000

    Adding assembly versions for GitVersion to replace

--- src/Serilog.Enrichers.CorrelationId/Properties/AssemblyInfo.cs
+++ src/Serilog.Enrichers.CorrelationId/Properties/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Serilog.Enrichers.CorrelationId.Tests")]
+// These are in here for GitVersion to overwrite
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
