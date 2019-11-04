f85d7b9c30582bd8030ed77a59c2d687344ca07e
Elliot Coyle <elliotcoyle@ekmsystems.co.uk>
2019-11-04 14:49:00 +0000

    Maybe its meant to be like this for the new CSProj?

--- src/Serilog.Enrichers.CorrelationId/Properties/AssemblyInfo.cs
+++ src/Serilog.Enrichers.CorrelationId/Properties/AssemblyInfo.cs
@@ -1,8 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Serilog.Enrichers.CorrelationId.Tests")]
-// These are in here for GitVersion to overwrite
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
